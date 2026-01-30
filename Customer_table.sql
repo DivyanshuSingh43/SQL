e2bdefcf6da96884cfb3fd2c81bf2a68a776ce39
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-01-30 16:45:23 +0530

    Add files via upload

---
+++ Customer_table.sql
@@ -0,0 +1,21 @@
+-- CREATE DATABASE Topper_record
+USE Topper_record;
+
+CREATE TABLE students_data (
+    order_id INT PRIMARY KEY,
+    customer_id VARCHAR(30),
+    amount INT,
+    city VARCHAR(50)
+);
+
+INSERT INTO students_data (order_id,customer_id,amount,city)
+VALUES
+(101, 'A01', 78, 'Pune'),
+(102, 'W21', 93,'Goa'),
+(103, 'B23', 85,'Goa'),
+(104, 'Chetan', 96,'Delhi'),
+(105, 'Rohit', 12,'Delhi'),
+(106, 'Raman', 82,'Delhi'),
+(107, 'Sid', 72,'Pune');
+
+SELECT * FROM students_data;
---
+++ Student_table.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE Topper_record;
+
+USE Topper_record;
+
+CREATE TABLE students (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    marks INT,
+    grade CHAR(1),
+    city VARCHAR(50)
+);
+
+INSERT INTO students (rollno, name, marks, grade, city)
+VALUES
+(101, 'Anil', 78, 'C', 'Pune'),
+(102, 'Aman', 93, 'A', 'Goa'),
+(103, 'Bhumi', 85, 'B', 'Goa'),
+(104, 'Chetan', 96, 'A', 'Delhi'),
+(105, 'Rohit', 12, 'F', 'Delhi'),
+(106, 'Raman', 82, 'B', 'Delhi'),
+(107, 'Sid', 72, 'B', 'Pune');
+
+SELECT * FROM students;
